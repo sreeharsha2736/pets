9b392a41a079647c92b0da0b7e61c24691c0bf93
sreeharsha2736 <sreeharsha.guntamadugu@gmail.com>
2024-04-05 16:00:52 -0400

    updating welcome name

--- js/home.js
+++ js/home.js
@@ -4,8 +4,9 @@ var currentUser = JSON.parse(sessionStorage.getItem("currentUser"));
 // Display the user's first name in navigation if logged in
 if (currentUser) {
   var userFirstName = document.getElementById("userFirstName");
-  userFirstName.textContent = currentUser.name.split(' ')[0];
-  userFirstName.style.display = "inline"; // Show the user's first name
+  userFirstName.textContent = "Welcome " + currentUser.name.split(' ')[0];
+  userFirstName.style.display = "inline";
+   // Show the user's first name
   document.getElementById("loginBtnContainer").style.display = "none"; // Hide signin button
   document.getElementById("logoutBtnContainer").style.display = "inline"; // Show logout button
 } else {
@@ -18,3 +19,5 @@ function handleLogoutButton() {
   sessionStorage.removeItem("currentUser"); // Remove user's session
   location.reload(); // Reload the page to reflect changes
 }
+
+console.log(localStorage);
